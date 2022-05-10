cf01a06c87094ce978b7f006981be9c2b77d81bd
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-10 21:01:37 +0700

    indexing user models

---
+++ src/api/users/models/index.js
@@ -0,0 +1,9 @@
+const UserLogin = require('./UserLogin');
+const UserPreRegister = require('./UserPreRegister');
+const UserRegistered = require('./UserRegistered');
+
+module.exports = {
+  UserLogin,
+  UserPreRegister,
+  UserRegistered,
+};
