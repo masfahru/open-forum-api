ed1a85fa6e1f0517113899f01a5b200b041f75ca
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-11 21:47:01 +0700

    add Authentication Token Manager Abstraction

---
+++ src/utils/security/abstracts/authenticationTokenManager.js
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+
+module.exports = class AuthenticationTokenManager {
+  async createRefreshToken(payload) {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async createAccessToken(payload) {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyRefreshToken(token) {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async decodePayload() {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
+};
