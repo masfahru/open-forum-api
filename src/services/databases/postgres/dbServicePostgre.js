b7affa5094786bdd6b9ed8c661859a584169a9b7
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-15 18:43:54 +0700

    Add info query to Error when failed sending query

--- src/services/databases/postgres/dbServicePostgre.js
+++ src/services/databases/postgres/dbServicePostgre.js
@@ -24,7 +24,7 @@ module.exports = class DbServicePostgre {
       const res = await this.#pool.query(text, values);
       return (({ rowCount, rows }) => ({ rowCount, rows }))(res);
     } catch (error) {
-      throw new DatabaseError(JSON.stringify(error));
+      throw new DatabaseError(`Query: ${text}\nError: ${JSON.stringify(error)}`);
     }
   }
 
