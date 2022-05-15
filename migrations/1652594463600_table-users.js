fa01ef139c48e2d6bd640128cc933b9ae2011872
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-15 17:50:22 +0700

    add user migration

---
+++ migrations/1652594463600_table-users.js
@@ -0,0 +1,34 @@
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    fullname: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
