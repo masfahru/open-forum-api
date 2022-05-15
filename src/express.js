eb769e7dea80b7fc9d6fb52ef57ca3a52d1c8539
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-16 04:59:23 +0700

    fixing in importing container

--- src/express.js
+++ src/express.js
@@ -1,6 +1,6 @@
 const { serverConfig } = require('./configs/server');
 const app = require('./server/express/createServer');
-const container = require('./container');
+const { container } = require('./container');
 
 app(container).listen(serverConfig.port, serverConfig.host, () => {
   console.log(`Server running in port: ${serverConfig.port}`);
