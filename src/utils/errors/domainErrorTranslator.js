c12da48af25c1bb94ac492cc01d1b7a74923b0dc
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-15 11:42:08 +0700

    add more error translations

--- src/utils/errors/domainErrorTranslator.js
+++ src/utils/errors/domainErrorTranslator.js
@@ -2,10 +2,14 @@ const InvariantError = require('./invariantError');
 
 module.exports = class DomainErrorTranslator {
   static directories = {
-    'REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'),
-    'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
-    'REGISTER_USER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
-    'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
+    'USER_PRE_REGISTER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'),
+    'USER_PRE_REGISTER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
+    'USER_PRE_REGISTER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
+    'USER_PRE_REGISTER.FULLNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter fullname melebihi batas limit'),
+    'USER_PRE_REGISTER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
+    'USER_PRE_REGISTER.PASSWORD_NOT_MEET_FORMAT': new InvariantError('tidak dapat membuat user baru karena password tidak memenuhi kriteria keamanan'),
+    'USER_REGISTERED.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('user properti yang dibutuhkan tidak ada'),
+    'USER_REGISTERED.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tipe data properti user tidak sesuai'),
     'USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
     'USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
     'REFRESH_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
