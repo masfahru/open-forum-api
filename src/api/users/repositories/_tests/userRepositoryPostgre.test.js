d82a38033ac48e96ded437ab5870aa8dabf6a1d0
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-19 10:48:41 +0700

    container should not used in testing repository

--- src/api/users/repositories/_tests/userRepositoryPostgre.test.js
+++ src/api/users/repositories/_tests/userRepositoryPostgre.test.js
@@ -1,121 +1,61 @@
 /* eslint-disable max-len */
-const { container } = require('../../../../container');
-const DbService = require('../../../../services/databases/abstracts/dbService');
-const { UserPreRegister } = require('../../models');
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const DbServicePostgre = require('../../../../services/databases/postgres/dbServicePostgre');
+const { testDbConfig } = require('../../../../configs/database');
 const UserRepositoryPostgre = require('../userRepositoryPostgre');
 const InvariantError = require('../../../../utils/errors/invariantError');
 
-describe('Test User Respository PostgreSQL', () => {
-  it('should return Invariant Error when username is exist', async () => {
-    const payload = {
-      username: 'fahru',
-      password: 'StrongPassword123#',
-      fullname: 'fahru',
-    };
+describe('Integration Test User Respository PostgreSQL', () => {
+  const pool = new Pool(testDbConfig);
+  const dbService = new DbServicePostgre(pool);
+  const userRepository = new UserRepositoryPostgre(dbService, nanoid);
 
-    // we only need to return this result from database
-    // for more information about this result, please refer to:
-    // https://node-postgres.com/api/result
-    const mockResolvedValue = {
-      rowCount: 1,
-      rows: [
-        { username: 'fahru' },
-      ],
+  // truncating table happens before and after all test
+  // so make sure to use unique username each test
+  beforeAll(async () => {
+    // should truncate the table first
+    await dbService.query({
+      text: 'TRUNCATE users CASCADE',
+      values: [],
+    });
+    // insert a user
+    const query = {
+      text: 'INSERT INTO users (id, username, password, fullname) VALUES ($1, $2, $3, $4)',
+      values: ['id-123-456-789-10', 'user-existUsername', 'hashed_password', 'fullname'],
     };
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockResolvedValue);
-
-    const mockUserRepository = new UserRepositoryPostgre(mockDbService, container.get('nanoid'));
-    const userPreRegister = new UserPreRegister(payload);
-    await expect(mockUserRepository.isUsernameUnique(userPreRegister)).rejects.toThrowError(InvariantError);
+    await dbService.query(query);
   });
 
-  it('should call query properly', async () => {
-    const userPreRegister = {
-      username: 'fahru',
-      password: 'hashed_password',
+  afterAll(async () => {
+    // Truncate user table
+    await dbService.query({
+      text: 'TRUNCATE users CASCADE',
+      values: [],
+    });
+  });
+
+  it('should return Invariant Error when username is exist', async () => {
+    const payload = {
+      username: 'user-existUsername',
+      password: 'hashed_password#',
       fullname: 'fahru',
     };
 
-    const mockResolvedValue = {
-      rowCount: 1,
-      rows: [
-        {
-          id: expect.any(String),
-          username: 'fahru',
-          fullname: 'fahru',
-        },
-      ],
-    };
-
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockResolvedValue);
-
-    const spy = jest.spyOn(mockDbService, 'query');
-
-    const mockUserRepository = new UserRepositoryPostgre(mockDbService, container.get('nanoid'));
-    await mockUserRepository.addUser(userPreRegister);
-    expect(spy).toHaveBeenCalled();
-    expect(spy).toHaveBeenCalledWith({
-      text: expect.any(String),
-      values: [
-        expect.any(String),
-        userPreRegister.username,
-        userPreRegister.password,
-        userPreRegister.fullname,
-      ],
-    });
+    await expect(userRepository.isUsernameUnique(payload)).rejects.toThrowError(InvariantError);
   });
 
   it('should return Expected Value when addUser is success', async () => {
     const payload = {
-      username: 'fahru',
-      password: 'StrongPassword123#',
+      username: 'user-fahru',
+      password: 'hashed_password#',
       fullname: 'fahru',
     };
 
-    const mockResolvedValue = {
-      rowCount: 1,
-      rows: [
-        {
-          id: 'user-123-456',
-          username: 'fahru',
-          fullname: 'fahru',
-        },
-      ],
-    };
-
-    const mockReturnValue = {
-      id: 'user-123-456',
-      username: 'fahru',
-      fullname: 'fahru',
-    };
-
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockResolvedValue);
-
-    const mockUserRepository = new UserRepositoryPostgre(mockDbService, container.get('nanoid'));
-    const userPreRegister = new UserPreRegister(payload);
-    await expect(mockUserRepository.addUser(userPreRegister)).resolves.toStrictEqual(mockReturnValue);
-  });
-
-  it('should return Invariant Error when failed adding user', async () => {
-    const payload = {
-      username: 'fahru',
-      password: 'StrongPassword123#',
-      fullname: 'fahru',
-    };
-
-    const mockRejectedValue = {
-      rowCount: 0,
-      rows: [],
-    };
-
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockRejectedValue);
-
-    const mockUserRepository = new UserRepositoryPostgre(mockDbService, container.get('nanoid'));
-    const userPreRegister = new UserPreRegister(payload);
-    await expect(mockUserRepository.addUser(userPreRegister)).rejects.toThrowError(InvariantError);
+    await expect(userRepository.addUser(payload)).resolves.toStrictEqual({
+      id: expect.any(String),
+      username: payload.username,
+      fullname: payload.fullname,
+    });
   });
 });
