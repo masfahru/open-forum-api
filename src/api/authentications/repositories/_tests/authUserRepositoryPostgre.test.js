af93c9111756c0fa54d3f8239af7e5e0fcc016b7
Imam Fahrur Rofi <imam@fahru.my.id>
2022-05-19 12:13:13 +0700

    integration test for Authentication User Repo

--- src/api/authentications/repositories/_tests/authUserRepositoryPostgre.test.js
+++ src/api/authentications/repositories/_tests/authUserRepositoryPostgre.test.js
@@ -1,52 +1,57 @@
-const DbService = require('../../../../services/databases/abstracts/dbService');
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const { testDbConfig } = require('../../../../configs/database');
+const DbService = require('../../../../services/databases/postgres/dbServicePostgre');
 const AuthenticationError = require('../../../../utils/errors/authenticationError');
-const AuthUserRepositoryPostgre = require('../authUserRepositoryPostgre');
+const AuthUserRepository = require('../authUserRepositoryPostgre');
 
 describe('Test UserRepository Postgre for Authentications', () => {
+  const pool = new Pool(testDbConfig);
+  const dbService = new DbService(pool);
+  const authUserRepository = new AuthUserRepository(dbService, nanoid);
+
+  // truncating table happens before and after all test
+  // so make sure to use unique username each test
+  beforeAll(async () => {
+    // should truncate the table first
+    await dbService.query({
+      text: 'TRUNCATE users CASCADE',
+      values: [],
+    });
+    // insert a user
+    const query = {
+      text: 'INSERT INTO users (id, username, password, fullname) VALUES ($1, $2, $3, $4)',
+      values: ['id-123-456-789-10', 'user-existUsername', 'hashed_password', 'fullname'],
+    };
+    await dbService.query(query);
+  });
+
+  afterAll(async () => {
+    // Truncate user table
+    await dbService.query({
+      text: 'TRUNCATE users CASCADE',
+      values: [],
+    });
+  });
+
   it('should throw 401 Error when username not found', async () => {
     const payload = {
       username: 'fahru',
       password: 'StrongP4ssw0rd$',
     };
 
-    const mockResolvedValue = {
-      rowCount: 0,
-      rows: [],
-    };
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockResolvedValue);
-
-    const mockRepository = new AuthUserRepositoryPostgre(mockDbService);
     // eslint-disable-next-line max-len
-    await expect(mockRepository.getPasswordByUsername(payload)).rejects.toThrowError(AuthenticationError);
+    await expect(authUserRepository.getPasswordByUsername(payload)).rejects.toThrowError(AuthenticationError);
   });
 
   it('should return required value if username is found', async () => {
     const payload = {
-      username: 'fahru',
-      password: 'StrongP4ssw0rd$',
+      username: 'user-existUsername',
     };
 
-    const mockResolvedValue = {
-      rowCount: 1,
-      rows: [
-        {
-          password: 'Hashed_Password',
-        },
-      ],
-    };
-    const mockDbService = new DbService();
-    mockDbService.query = jest.fn().mockResolvedValue(mockResolvedValue);
-
-    const mockRepository = new AuthUserRepositoryPostgre(mockDbService);
-    const result = await mockRepository.getPasswordByUsername(payload);
+    const result = await authUserRepository.getPasswordByUsername(payload);
     expect(result).toEqual({
-      password: 'Hashed_Password',
-    });
-    expect(mockDbService.query).toHaveBeenCalledTimes(1);
-    expect(mockDbService.query).toHaveBeenCalledWith({
-      text: expect.any(String),
-      values: expect.any(Array),
+      password: expect.any(String),
     });
   });
 });
